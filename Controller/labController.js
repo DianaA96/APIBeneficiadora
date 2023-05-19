5d0fa74b185092254705d76f194b5b6999332791
jose heron samperio leon <joseheronsamperio@gmail.com>
2023-05-19 11:17:19 -0600

    xd

--- Controller/labController.js
+++ Controller/labController.js
@@ -9,97 +9,46 @@ connection.connect(error => {
     console.log('Conected Lab');
 });
 
-/* Este codigo genera los reportes y luego lo relaciona con las tablas 
-module.exports.LabReport = (request, response) => {
-    try {
-        var bod = request.body;
-        const turnos = ["primerT", "segundoT", "tercerT"];
-        var q = 1;
-        var times = 0;
-        turnos.forEach(turno => {
-            var i = q++;
-            var sql1 = `
-            INSERT INTO Analisis(idUsuario,idMina,idPlanta,fechaMuestreo,fechaEnsaye,turno) 
-            values(${bod.idUsuario},${bod.idMina},${bod.idPlanta},'${bod.fechaMuestreo}','${bod.fechaEnsaye}',${(q - 1).toString()});`;
-            connection.query(sql1, (error, rows) => {
-                if (error) {
-                    console.error('An error occurred:', error);
-                } else {
-                    times++;
-                    console.log("-----------------------------times: ", times);
-                    let idAnalisis = rows.insertId; //Guardas el id nuevo que es el id del reporte nuevo generado
-                    //El primer for con la funcion Object Keys lee los concentrados por nombre y los convierte en id 
-                    for (let i = 0; i < Object.keys(bod[turno]).length; i++) {
-                        let concentrado = Object.keys(bod[turno])[i]
-                        //El segundo for con la funcion Object Keys lee los elementos por nombre y los convierte en id
-                        console.log("Concentrado: ", concentrado)
-                        for (let j = 0; j < Object.keys(bod[turno][concentrado]).length; j++) {
-                            let elemento = Object.keys(bod[turno][concentrado])[j] //Guarda el elemento que es
-                            let porcentaje = bod[turno][concentrado][elemento] //Guarda el porcentaje del elemento
-                            console.log(elemento, ": ", porcentaje);
-                            var sqlIdC = `
-                                INSERT INTO Laboratorio(idAnalisis,idConcentrado,idElemento,gton) 
-                                VALUES(${idAnalisis},(SELECT idConcentrado FROM Concentrado where nombre = '${concentrado}'),
-                                (SELECT idElemento FROM Elemento where nombre = '${elemento}'),${porcentaje})`;
-                            connection.query(sqlIdC, (error, rows) => {
-                                if (error) {
-                                    // Handle the error
-                                    console.error('An error occurred:', error);
-                                }
-                                else { 
-                                    console
-                                }
-                            });
-                        }
-                    }
-                }
-            })
-        });
-    } catch (e) {
-        console.log(e)
-    }
-};
-de cada concentrado y a su vez con cada uno de los elementos */
-
+//Genera el reporte de laboratorio
 module.exports.LabReport = async (request, response) => {
     try {
         var bod = request.body;
         const turnos = ["primerT", "segundoT", "tercerT"];
-        var q = 1;
-        var times = 0;
+        var q = 0;
         let idAnalisis;
 
         for (const turno of turnos) {
             q++;
             var sql1 = `INSERT INTO Analisis(idUsuario,idMina,idPlanta,fechaMuestreo,fechaEnsaye,turno) 
-        values(${bod.idUsuario},${bod.idMina},${bod.idPlanta},'${bod.fechaMuestreo}','${bod.fechaEnsaye}',${(q - 1).toString()});`;
+        values(${bod.idUsuario},${bod.idMina},${bod.idPlanta},'${bod.fechaMuestreo}','${bod.fechaEnsaye}',${(q).toString()});`;
 
-            await new Promise((resolve, reject) => {
+            await new Promise((resolve, reject) => {//espera al insert de analisis para obtener el idAnalisis
                 connection.query(sql1, (error, rows) => {
                     if (error) {
                         console.error('An error occurred:', error);
                         reject(error);
                     } else {
-                        times++;
                         idAnalisis = rows.insertId;
-                        console.log("-----------------------------times: ", times);
                         resolve(idAnalisis);
                     }
                 });
             });
 
-            for (let i = 0; i < Object.keys(bod[turno]).length; i++) {
-                let concentrado = Object.keys(bod[turno])[i];
-                console.log(concentrado)
-                for (let j = 0; j < Object.keys(bod[turno][concentrado]).length; j++) {
-                    let elemento = Object.keys(bod[turno][concentrado])[j];
-                    let porcentaje = bod[turno][concentrado][elemento];
-                    console.log(elemento, ": ", porcentaje);
+            for (let i = 0; i < Object.keys(bod[turno]).length; i++) {//intera dependiendo de la cantidad de concentrados Cabeza,colas,etc
+
+                let concentrado = Object.keys(bod[turno])[i];//lee el concentrado
+
+                for (let j = 0; j < Object.keys(bod[turno][concentrado]).length; j++)//itera dependiendo de la cantidad de elementos
+                {
+                    let elemento = Object.keys(bod[turno][concentrado])[j];//lee el elemento
+                    let porcentaje = bod[turno][concentrado][elemento];//lee el porcentaje
+
                     var sqlIdC = `INSERT INTO Laboratorio(idAnalisis,idConcentrado,idElemento,gton) 
                                   VALUES(${idAnalisis},(SELECT idConcentrado FROM Concentrado where nombre = '${concentrado}'),
                                   (SELECT idElemento FROM Elemento where nombre = '${elemento}'),${porcentaje})`;
 
-                    await new Promise((resolve, reject) => {
+                    await new Promise((resolve, reject) =>//espera a que se realize el insert de laboratorio para cada porcentaje del elemento iterado
+                    {
                         connection.query(sqlIdC, (error, rows) => {
                             if (error) {
                                 console.error('An error occurred:', error);
@@ -118,9 +67,10 @@ module.exports.LabReport = async (request, response) => {
     }
 };
 
-
+//Mestra el reporte de laboratorio segun la fecha enviada
 module.exports.LabTable = (req, res) => {
-    const query = `SELECT 
+    try {
+        const query = `SELECT 
                     Analisis.idAnalisis AS idAnalisis,
                     Concentrado.nombre AS nombre_concentrado,
                     Elemento.nombre AS nombre_elemento,
@@ -150,10 +100,14 @@ module.exports.LabTable = (req, res) => {
                     Analisis.idAnalisis
                 `;
 
-    connection.query(query, (err, result) => {
-        if (err) {
-            throw err;
-        }
-        res.send(result);
-    });
+        connection.query(query, (err, result) => {
+            if (err) {
+                throw err;
+            } else {
+                res.send(result);
+            }
+        });
+    } catch (e) {
+        console.error(e);
+    };
 }
