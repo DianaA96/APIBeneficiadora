2aa428f22599fc52fd39b19e55873bcdb7492b68
MariaFernanda2502 <ferchasalvador25@gmail.com>
2023-05-25 11:12:16 -0600

    Gerente v2

--- Routes/Gerente.js
+++ Routes/Gerente.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 var controller = require ('../Controller/gerenteController')
 
-router.get('/historial', controller.Historial)
+router.get('/movMineral', controller.movMineral)
+// router.get('/embarque', embarque)
+router.get('/grapHistoricas', controller.grapHistoricas)
+router.get('/historialBascula', controller.historialBascula)
 
 module.exports = router
