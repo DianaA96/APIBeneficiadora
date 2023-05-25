d673a4b939639255b9539bef3713259e0a9a305d
jose heron samperio leon <joseheronsamperio@gmail.com>
2023-05-25 15:23:10 -0600

    cambio query lablist

--- Controller/labController.js
+++ Controller/labController.js
@@ -115,14 +115,16 @@ module.exports.LabTable = async (req, res) => {
 
 module.exports.LabList = async (req, res) => {
     try {
-        const query = `SELECT idAnalisis AS ID, 
-                        nombre AS nombreMina,
-                        fechaEnsaye,
-                        fechaMuestreo
-                        FROM analisis 
-                        INNER JOIN Mina ON mina.idMina=analisis.idMina
-                        WHERE tms IS NULL
-                        group by fechaEnsaye`;
+
+        const query = `SELECT analisis.idAnalisis AS ID,
+                        Mina.nombre AS nombreMina,
+                        analisis.fechaEnsaye,
+                        analisis.fechaMuestreo
+                        FROM analisis
+                        INNER JOIN Mina ON Mina.idMina = analisis.idMina
+                        WHERE analisis.tms IS NULL
+                        GROUP BY analisis.idAnalisis, Mina.nombre, analisis.fechaEnsaye, analisis.fechaMuestreo;
+`;
 
         await new Promise((resolve, reject) => {
             connection.query(query, (err, result) => {
