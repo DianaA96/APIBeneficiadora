0b3a50498b8b9278f6f4a3ff13649c85e071b82d
MariaFernanda2502 <ferchasalvador25@gmail.com>
2023-06-05 23:09:56 -0600

    Arreglos en balance

--- Controller/gerenteController.js
+++ Controller/gerenteController.js
@@ -8,409 +8,54 @@ connection.connect(error => {
     console.log('Conected gerente'); 
 });
 
-/*module.exports.movMineral = (request, response) => {
-    // CONSULTA PARA ACARRADAS
-    var acarreo = `SELECT 
-                    nombre,
-                    SUM(acarreo) AS 'Acarreo total'
-                  FROM mina 
-                    JOIN acarreo USING(idMina)
-                    JOIN movimiento_mineral USING(idMovimiento)
-                  WHERE 
-                    MOVIMIENTO_MINERAL.fecha = '${request.query.fecha}'
-                  GROUP BY (idMina)`;
-  
-    connection.query(acarreo, (error, rows1) => {
-      if (error) {
-        response.send(error);
-        return;
-      }
-  
-      // CONSULTA PARA TRITURADAS
-      var trituradas = `SELECT 
-                          nombre,
-                          SUM(trituradas) AS 'Trituradas total'
-                      FROM mina 
-                          JOIN trituradas USING(idMina)
-                          JOIN movimiento_mineral USING(idMovimiento)
-                      WHERE MOVIMIENTO_MINERAL.fecha = '${request.query.fecha}'
-                      GROUP BY (idMina)`;
-  
-      connection.query(trituradas, (error, rows2) => {
-        if (error) {
-          response.send(error);
-          return;
-        }
-  
-        // OBJETO POR MINA
-        var combinedRows = [];
-  
-        for (let i = 0; i < rows1.length; i++) {
-          var mina = rows1[i].nombre;
-          var acarreoTotal = rows1[i]['Acarreo total'];
-          var trituradasTotal = rows2[i]['Trituradas total'];
-          var existenciaPatios = acarreoTotal - trituradasTotal;
-          var existenciaInicial = existenciaPatios + acarreoTotal;
-  
-          combinedRows[i] = {
-            nombre: mina,
-            acarreo: acarreoTotal,
-            trituradas: trituradasTotal,
-            existenciaPatios: existenciaPatios,
-            existenciaInicial: existenciaInicial,
-          };
-        }
-
-        // ENVÍO DE RESPUESTA HTTP
-        response.json(combinedRows);
-      });
-    });
-  };  
-
-  module.exports.embarque = (request, res) => {
-    const query = `SELECT 
-                        MINA.nombre AS mina, 
-                        CONCENTRADO.nombre as concentrado, 
-                        SUM(embarque) AS total
-                    FROM 
-                        mina 
-                        JOIN embarque USING(idMina)
-                        JOIN concentrado USING(idConcentrado)
-                    WHERE
-                        EMBARQUE.fecha = '${request.query.fecha}'
-                    GROUP BY 
-                        MINA.idMina,
-                        CONCENTRADO.idConcentrado`
-
-    connection.query(query, (err, rows) => {
-        if (err) {
-            throw err;
-        }
-        
-        // OBJETO DE RESULTADO
-        const result = {};
-
-        rows.forEach(row => {
-            const mina = row.mina;
-            const concentrado = row.concentrado;
-            const total = row.total;
-            
-            // VERIFICA SI LA MINA EXISTE EN EL OBJETO
-            if (!result.hasOwnProperty(mina)) {
-                // CREA UN NUEVO OBJETO PARA LA MINA
-                result[mina] = {};
-            }
-
-            // VERIFICA SI EL CONCENTRADO EXISTE EN EL OBJETO DE LA MINA
-            if (!result[mina].hasOwnProperty(concentrado)) {
-                // AGREGA UNA NUEVA PROPIEDAD PARA EL CONCENTRADO
-                result[mina][concentrado] = 0;
-            }
-
-            // SUMA EL TOTAL AL VALOR EXISTENTE
-            result[mina][concentrado] += total;
-        });
-
-        // RESPUESTA
-        res.send(result);
-    });
-};
-
-// FILTRO DE AÑO Y MINA
-// CAMBIAR ID POR NOMBRE 
-module.exports.grapHistoricas = (request, response) =>{
-    // CONSULTA PARA ACARRADAS
-    var acarreo =   `SELECT 
-                        idMina,
-                        MONTH(fecha) AS mes, 
-                        SUM(acarreo) AS totalAcarreo
-                    FROM 
-                        acarreo
-                    WHERE 
-                        fecha IS NOT NULL
-                    GROUP BY 
-                        MONTH(fecha),
-                        idMina`;
-
-    connection.query(acarreo, (error, rows1) => {
-        if (error) {
-            response.send(error);
-            return;
-        }
-
-        // CONSULTA PARA TRITURADAS
-        var trituradas =   `SELECT 
-                                idMina,
-                                MONTH(fecha) AS mes, 
-                                SUM(trituradas) AS totalTrituradas
-                            FROM 
-                                trituradas
-                            WHERE 
-                                fecha IS NOT NULL
-                            GROUP BY 
-                                MONTH(fecha),
-                                idMina`;
-
-        connection.query(trituradas, (error, rows2) => {
-            if (error) {
-                response.send(error);
-                return;
-            }
-
-            // CONSULTA PARA CONCENTRADOS
-            var concentrados =  `SELECT 
-                                    MONTH(fecha) AS mes, 
-                                    SUM(embarque) AS totalConcentrados,
-                                    CONCENTRADO.nombre
-                                FROM 
-                                    embarque
-                                    JOIN concentrado USING(idConcentrado)
-                                    JOIN mina USING(idMina)
-                                WHERE
-                                    FECHA IS NOT NULL
-                                GROUP BY 
-                                    MONTH(fecha),
-                                    EMBARQUE.idConcentrado`;
-
-            connection.query(concentrados, (error, rows3) => {
-                if (error) {
-                    response.send(error);
-                    return;
-                }
-    
-                // OBJETO QUE SE RETORNA
-                var combinedRows = {
-                    acarreo: rows1,
-                    trituradas: rows2,
-                    concentrados: rows3
-                };
-    
-                // ENVÍO DE RESPUESTA HTTP
-                response.json(combinedRows);
-            });
-        });
-    });
-};
-
-module.exports.historialBascula = (request, response) =>{
-    // CONSULTA PARA ACARRADAS
-    var acarreo =   `SELECT 
-                        nombre,
-                        SUM(acarreo) AS 'Acarreo total'
-                    FROM mina 
-                        JOIN acarreo USING(idMina)
-                        JOIN movimiento_mineral USING(idMovimiento)
-                    WHERE MOVIMIENTO_MINERAL.fecha = '${request.query.fecha}'
-                    GROUP BY (nombre)`;
-
-    connection.query(acarreo, (error, rows1) => {
-        if (error) {
-            response.send(error);
-            return;
-        }
-
-        // CONSULTA PARA TRITURADAS
-        var trituradas =    `SELECT 
-                                nombre,
-                                SUM(trituradas) AS 'Trituradas total'
-                            FROM mina 
-                                JOIN trituradas USING(idMina)
-                                JOIN movimiento_mineral USING(idMovimiento)
-                            WHERE MOVIMIENTO_MINERAL.fecha = '${request.query.fecha}'
-                            GROUP BY (nombre)`
-
-        connection.query(trituradas, (error, rows2) => {
-            if (error) {
-                response.send(error);
-                return;
-            }
-
-            // CALCULOS
-            var existenciaPatios = rows1[0]['Acarreo total'] - rows2[0]['Trituradas total'];
-            var existenciaInicial = existenciaPatios + rows1[0]['Acarreo total'];
-
-            // OBJETO QUE SE RETORNA
-            var combinedRows = {
-                acarreo: rows1,
-                trituradas: rows2,
-                existenciaPatios: existenciaPatios,
-                existenciaInicial: existenciaInicial
-            };
-
-            // ENVÍO DE RESPUESTA HTTP
-            response.json(combinedRows);
-        });
-    });
-}; 
-
-
-/// CORREGIR FORMATO COMO EL DE VERO VERO
-module.exports.balance = (request, res) => {
-    const concentradoQuery = `SELECT 
-                                idElemento, 
-                                idConcentrado, 
-                                ELEMENTO.nombre AS elemento,
-                                CONCENTRADO.nombre AS concentrado,
-                                SUM(gton) AS cantidad, 
-                                tms
-                              FROM 
-                                analisis
-                                JOIN laboratorio USING(idAnalisis)
-                                JOIN elemento USING(idElemento)
-                                JOIN concentrado USING(idConcentrado)
-                              WHERE 
-                                idAnalisis = 1 
-                                AND idConcentrado = 5
-                              GROUP BY 
-                                idElemento, 
-                                idConcentrado`;
-  
-    connection.query(concentradoQuery, (error, rows1) => {
-      if (error) {
-        res.send(error);
-        return;
-      }
-  
-      // OBJETO POR CONCENTRADO
-      const combinedRows = {};
-  
-      for (let i = 0; i < rows1.length; i++) {
-        const concentrado = rows1[i].concentrado;
-        const elemento = rows1[i].elemento;
-        const cantidad = rows1[i].cantidad;
-        const tms = rows1[i].tms;
-  
-        if (!combinedRows.hasOwnProperty(concentrado)) {
-          combinedRows[concentrado] = {
-            tms: tms,
-            elementos: []
-          };
-        }
-  
-        combinedRows[concentrado].elementos.push({
-          elemento: elemento,
-          analisis: cantidad
-        });
-      }
-
-      console.log(concentradoQuery)
-  
-      // ENVÍO DE RESPUESTA HTTP
-      res.json(combinedRows);
-    });
-  };
-
-  module.exports.balance = (request, res) => {
-    const concentradoQuery = `SELECT 
-                                idElemento, 
-                                idConcentrado, 
-                                ELEMENTO.nombre AS elemento,
-                                CONCENTRADO.nombre AS concentrado,
-                                SUM(gton) AS cantidad, 
-                                tms
-                              FROM 
-                                analisis
-                                JOIN laboratorio USING(idAnalisis)
-                                JOIN elemento USING(idElemento)
-                                JOIN concentrado USING(idConcentrado)
-                              WHERE 
-                                idAnalisis = 1 
-                                AND idConcentrado = 5
-                              GROUP BY 
-                                idElemento, 
-                                idConcentrado`;
-  
-    connection.query(concentradoQuery, (error, rows1) => {
-      if (error) {
-        res.send(error);
-        return;
-      }
-  
-      // OBJETO POR CONCENTRADO
-      const combinedRows = {};
-  
-      for (let i = 0; i < rows1.length; i++) {
-        const concentrado = rows1[i].concentrado;
-        const elemento = rows1[i].elemento;
-        const cantidad = rows1[i].cantidad;
-        const tms = rows1[i].tms;
-  
-        if (!combinedRows.hasOwnProperty(concentrado)) {
-          combinedRows[concentrado] = {
-            tms: tms,
-            elementos: []
-          };
-        }
-  
-        combinedRows[concentrado].elementos.push({
-          elemento: elemento,
-          analisis: cantidad
-        });
-      }
-
-      console.log(concentradoQuery)
-  
-      // ENVÍO DE RESPUESTA HTTP
-      res.json(combinedRows);
-    });
-  };*/
-  
-  module.exports.balance = (request, response) => {
-    var sql = `SELECT 
+module.exports.balance = (request, response) => {
+  var sql = `SELECT 
                 CONCENTRADO.nombre AS concentrado, 
                 ELEMENTO.nombre AS elemento, 
                 gtonR,
-                MAX(tms)
+                tms
               FROM 
                 elemento JOIN reporte USING(idElemento)
                 JOIN concentrado USING(idConcentrado)
               WHERE 
-                fecha = '${request.query.fecha}'
-              GROUP BY 
-                CONCENTRADO.nombre,
-                ELEMENTO.nombre`
+                fecha = '${request.query.fecha}' AND
+                CONCENTRADO.nombre = 'Pb' OR
+                CONCENTRADO.nombre = 'Zn' OR 
+                CONCENTRADO.nombre = 'Cabeza' OR
+                CONCENTRADO.nombre = 'Colas'`
 
-    connection.query(sql, (error, rows) => {
-        if (error) { 
-          response.send(error);
-          return;
-        }
+  connection.query(sql, (error, rows) => {
+    if (error) {
+      response.send(error);
+    }
 
-        // OBJETO DE RESULTADO
-        const result = {};
-        let tms = 0;
+    const result = {};
 
-        rows.forEach(row => {
-            const concentrado = row.concentrado;
-            const elemento = row.elemento;
-            const total = row.gtonR;
-            tms = row.tms;
+    rows.forEach(row => {
+      const concentrado = row.concentrado;
+      const elemento = row.elemento;
+      const total = row.gtonR;
+      const tms = row.tms;
 
-            // VERIFICA SI LA MINA EXISTE EN EL OBJETO
-            if (!result.hasOwnProperty(concentrado)) {
-                // CREA UN NUEVO OBJETO PARA LA MINA
-                result[concentrado] = {};
-            }
+      // VERIFICA SI CONCENTRADO EXISTE
+      if (!result.hasOwnProperty(concentrado)) {
+        // OBJETO PARA CONCENTRADO
+        result[concentrado] = {
+          tms: tms
+        };
+      }
 
-            // VERIFICA SI EL CONCENTRADO EXISTE EN EL OBJETO DE LA MINA
-            if (!result[concentrado].hasOwnProperty(elemento)) {
-                // AGREGA UNA NUEVA PROPIEDAD PARA EL CONCENTRADO
-                result[concentrado][elemento] = 0;
-            }
+      // VERIFICA SI CONCENTRADO EXISTE
+      if (!result[concentrado].hasOwnProperty(elemento)) {
+        // ELEMENTO PARA ELEMENTO
+        result[concentrado][elemento] = total;
+      }
+    });
 
-            // SUMA EL TOTAL AL VALOR EXISTENTE
-            result[concentrado][elemento] += total;
-        });
-
-        result.tms = tms;
-
-        // RESPUESTA
-        response.send(result);
-      });
+    response.json(result);
+  });
 };
 
-
-// CORREGIR
 module.exports.movMineral = (request, response) => {
   var query = `SELECT 
                   MINA.nombre,
@@ -439,6 +84,7 @@ module.exports.movMineral = (request, response) => {
       var trituradasTotal = rows[i]['trituradas1'] + rows[i]['trituradas2'];
       var existenciaPatios = acarreoTotal - trituradasTotal;
       var existenciaInicial = existenciaPatios + acarreoTotal;
+      // ACUMULADO DEL MES 
 
       combinedRows[i] = {
         nombre: mina,
@@ -500,6 +146,28 @@ module.exports.embarque = (request, response) => {
   });
 };
 
+// ARREGLO DEMINAS
+// OBJETO X MES 
+
+/*
+en orden de minas
+  trituradas [
+    1 [45, 67, 43],
+    2
+    3
+  ]
+  acarreo [
+    2 [45, 67, 43],
+    4
+    5
+  ]
+  en orden de ...
+  concentrado [
+    3 [45, 67, 43],
+    6
+    7
+  ]
+*/
 module.exports.grapHistoricas = (request, response) => {
   // CONSULTA PARA ACARRADAS Y TRITURADAS
   var query = `SELECT 
